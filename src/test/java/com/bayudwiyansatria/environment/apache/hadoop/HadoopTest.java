ec626df0f6fa5203e3b4ee8d4c087b2b893b5165
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-11-27 17:23:17 +0700

    Edit Workflow and add test

--- src/test/java/com/bayudwiyansatria/environment/apache/hadoop/HadoopTest.java
+++ src/test/java/com/bayudwiyansatria/environment/apache/hadoop/HadoopTest.java
@@ -1,7 +1,13 @@
 package com.bayudwiyansatria.environment.apache.hadoop;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HadoopTest {
-	
+	@Test
+	public void Hadoop(){
+		double[][] data = new com.bayudwiyansatria.io.IO().readCSV_double ( "src/main/resources/ruspini" );
+		new com.bayudwiyansatria.mat.Mat ().print ( data );
+	}
 }
